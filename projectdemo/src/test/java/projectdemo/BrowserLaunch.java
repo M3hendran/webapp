0ea13b844b86a61f6d7647be6baed2dddcaed4d8
Mahendran <HP@192.168.2.5>
2020-07-15 23:49:28 +0530

    Files modified

---
+++ projectdemo/src/test/java/projectdemo/BrowserLaunch.java
@@ -0,0 +1,31 @@
+package projectdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class BrowserLaunch {
+	
+	private static WebDriver driver;
+	
+	@Test
+	public static void browserLaunch() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "Browsers/chromedriver.exe");
+		
+		
+		
+		driver = new ChromeDriver();
+		driver.get("https://www.orangecrm.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		System.out.println(driver.getTitle());
+	}
+	
+	@Test
+	public static void browserQuit() throws Exception {
+		
+		driver.quit();
+	}
+}
