258b3eab0724be781e875b7120ccb6cf7aabdc0b
Mahendran <HP@DESKTOP-UEC7B3F>
2020-07-26 07:07:47 +0530

    groovy file generated

---
+++ webapp/webapp.groovy
@@ -0,0 +1,26 @@
+import org.hamcrest.Description
+
+job ("Automating job with DSL Plugin"){
+	
+		Description("This script will automatically create a jenkins job")
+	
+	scm {
+		
+		git("https://github.com/M3hendran/webapp.git","master")
+	}
+	
+	triggers {
+		
+		scm("* * * * *")
+	}
+	
+	steps {
+		
+		maven("clean package","webapp/pom.xml")
+	}
+	
+	publishers {
+		
+		archiveArtifacts "**/*.war"
+	}
+}
