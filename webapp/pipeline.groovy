bce41d6ccd2d6a00a901b29e616ddf165937bca7
Mahendran <HP@192.168.2.6>
2020-07-31 19:11:02 +0530

    Created pipeline script

---
+++ webapp/pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline {
+	
+	description ("This is pipeline script for an agent")
+	
+	agent any
+	
+	stages{
+		
+		stage("Init"){
+			
+			steps {
+				
+				echo "Executing initilization of script..."
+			}
+		}
+		stage("Staging"){
+			
+			steps{
+				
+				echo "Executing staging script..."
+			}
+		}
+	
+		stage ("Production"){
+			
+			steps {
+				
+				echo "Executing production script..."
+			}
+		}
+}
